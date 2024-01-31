484aff7963888eac1ecc8e31086aa3f601854cdd
Yoneyama Rise <rise.yoneyama@gmail.com>
2024-02-01 01:16:09 +0900

    typoを修正

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 curl -o cloud-sql-proxy https://storage.googleapis.com/cloud-sql-connectors/cloud-sql-proxy/v2.6.1/cloud-sql-proxy.linux.amd64
 
 # ↑を実行可能に
-chmod +x cloud_sql_proxy
+chmod +x cloud-sql-proxy
 
 # プロキシサーバを起動(起動後別のシェルでMySQLサーバにプロキシサーバ経由で接続する)
-./cloud_sql_proxy 'meta-leaf-412610:asia-northeast1:disaster-info-db?port=$PORT' --credentials-file='$GOOGLE_APPLICATION_CREDENTIALS'
+./cloud-sql-proxy 'meta-leaf-412610:asia-northeast1:disaster-info-db?port=$PORT' --credentials-file='$GOOGLE_APPLICATION_CREDENTIALS'
 
 yarn install
 
