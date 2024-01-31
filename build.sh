36bb38e27f38c7ba9c765ff561325a0bb95f3718
Yoneyama Rise <rise.yoneyama@gmail.com>
2024-02-01 01:18:27 +0900

    PORT を修正

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ curl -o cloud-sql-proxy https://storage.googleapis.com/cloud-sql-connectors/clou
 chmod +x cloud-sql-proxy
 
 # プロキシサーバを起動(起動後別のシェルでMySQLサーバにプロキシサーバ経由で接続する)
-./cloud-sql-proxy 'meta-leaf-412610:asia-northeast1:disaster-info-db?port=$PORT' --credentials-file='$GOOGLE_APPLICATION_CREDENTIALS'
+./cloud-sql-proxy 'meta-leaf-412610:asia-northeast1:disaster-info-db?port=2525' --credentials-file='$GOOGLE_APPLICATION_CREDENTIALS'
 
 yarn install
 
