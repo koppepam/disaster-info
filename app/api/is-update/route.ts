04d5cb523fb30b9e36b1b764a3ac998ad7cbe3e3
Yoneyama Rise <rise.yoneyama@gmail.com>
2024-02-03 16:51:52 +0900

    api/is-update を高頻度フィードに変更

--- app/api/is-update/route.ts
+++ app/api/is-update/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import * as xml2js from 'xml2js';
 
 export async function GET(request: NextRequest) {
-  const response = await fetch(`https://www.data.jma.go.jp/developer/xml/feed/eqvol_l.xml`); // データ
+  const response = await fetch(`https://www.data.jma.go.jp/developer/xml/feed/eqvol.xml`); // データ
   const xml = await response.text();
   const parser = new xml2js.Parser({ explicitArray: false });
   const { feed } = await parser.parseStringPromise(xml);
