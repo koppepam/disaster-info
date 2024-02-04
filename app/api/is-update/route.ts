9f0d0ddb70b811b122ee305d2f5adcd214138ff4
Yoneyama Rise <rise.yoneyama@gmail.com>
2024-02-04 19:57:56 +0900

    revalidate を 0に指定

--- app/api/is-update/route.ts
+++ app/api/is-update/route.ts
@@ -1,8 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
 import * as xml2js from 'xml2js';
 
-export const runtime = 'edge';
-// export const revalidate = 0;
+// export const runtime = 'edge';
+export const revalidate = 0;
 
 export async function GET(request: NextRequest) {
   const response = await fetch(`https://www.data.jma.go.jp/developer/xml/feed/eqvol.xml`, { cache: "no-store" }); // データ
