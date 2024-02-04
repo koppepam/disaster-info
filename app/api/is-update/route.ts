a999cecb31ebe1dd39d4758f2df43e82f1565211
Yoneyama Rise <rise.yoneyama@gmail.com>
2024-02-04 19:55:13 +0900

    runtimeをedgeに指定

--- app/api/is-update/route.ts
+++ app/api/is-update/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import * as xml2js from 'xml2js';
 
-// export const runtime = 'edge';
+export const runtime = 'edge';
 // export const revalidate = 0;
 
 export async function GET(request: NextRequest) {
